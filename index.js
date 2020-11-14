40a31e511da88bc75c6578011e7b403e007c8016
wlytle <wlytle89@gmail.com>
2020-11-14 14:39:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, type) {
+    let thankYou = []
+    for (let i=0; i<names.length; i++) {
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`)
+    }
+    return thankYou
+}
+
+function countDown(num) {
+    while (num >= 0 ) {
+        console.log(num);
+        num --;
+    }
+}
